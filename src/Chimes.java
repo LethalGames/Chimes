45b85387c0b557754238de55d72262de0c425285
mrpanderson <panderson@nps.org>
2016-06-07 09:24:07 -0400

    Added comments
    Added several comments for testing purposes

--- src/Chimes.java
+++ src/Chimes.java
@@ -1,5 +1,7 @@
 /**
  * Created by Derek on 6/3/2016.
  */
+ 
+ // Comment by Mr. Anderson
 public class Chimes {
 }
--- src/Sound.java
+++ src/Sound.java
@@ -2,4 +2,6 @@
  * Created by Derek on 6/3/2016.
  */
 public class Sound {
+	private int num; // Added by Mr. Anderson
 }
+// After new branch added
