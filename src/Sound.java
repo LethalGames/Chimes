9e145c2ab7dbfb942f053a378829460602203e63
LethalGames <deeaath92@gmail.com>
2016-06-07 08:35:32 -0400

    Made playSound static

--- src/Sound.java
+++ src/Sound.java
@@ -2,14 +2,14 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 public class Sound {
-    public void playSound(final String input)
+    public static void playSound(final String input)
     {
         new Thread(() ->
         {
             try
             {
                 Clip c = AudioSystem.getClip();
-                File sound = new File("C:\\Users\\Derek\\IdeaProjects\\Player\\out\\artifacts\\Player_jar\\sounds\\" + input);
+                File sound = new File("P:\\" + input);
                 AudioInputStream ain = AudioSystem.getAudioInputStream(sound);
                 c.open(ain);
                 c.start();
