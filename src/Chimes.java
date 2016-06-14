541244f0ba1a8f661e4eff8c5d5ed3a86c5126d0
LethalGames <deeaath92@gmail.com>
2016-06-14 13:36:31 -0400

    Made Chimes an object

--- src/Chimes.java
+++ src/Chimes.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
 public class Chimes {
-    private static int[][] days = new int[][]{new int[]{0, 1, 2, 3, 4}, new int[]{5, 6, 0, 1, 2}, new int[]{3, 4, 5, 6, 0}, new int[]{1, 2, 3, 4, 5}, new int[]{6, 0, 1, 2, 3}, new int[]{4, 5, 6, 0, 1}, new int[]{2, 3, 4, 5, 6}};
-    private static double[] startTimes = new double[]{7.25, 8.40, 9.49, 10.58, 12.54}, endTimes = new double[]{8.30, 9.45, 10.54, 12.50, 13.55}, altStartTimes = new double[]{7.25, 8.30, 9.24, 10.04, 10.58, 12.54}, altEndTimes = new double[]{8.15, 9.20, 10.00, 10.54, 12.50, 13.55};
-    private static boolean isAlt;
-    private static int currentDay;
-    private static String summary;
-    private static ArrayList<Integer>[] alarmTimes = new ArrayList[7];
-    public static void main(String[] args) throws InterruptedException {
+    private int[][] days = new int[][]{new int[]{0, 1, 2, 3, 4}, new int[]{5, 6, 0, 1, 2}, new int[]{3, 4, 5, 6, 0}, new int[]{1, 2, 3, 4, 5}, new int[]{6, 0, 1, 2, 3}, new int[]{4, 5, 6, 0, 1}, new int[]{2, 3, 4, 5, 6}};
+    private double[] startTimes = new double[]{7.25, 8.40, 9.49, 10.58, 12.54}, endTimes = new double[]{8.30, 9.45, 10.54, 12.50, 13.55}, altStartTimes = new double[]{7.25, 8.30, 9.24, 10.04, 10.58, 12.54}, altEndTimes = new double[]{8.15, 9.20, 10.00, 10.54, 12.50, 13.55};
+    private boolean isAlt;
+    private int currentDay;
+    private String summary;
+    private ArrayList<Integer>[] alarmTimes = new ArrayList[7];
+    public void Chimes() {
         Calendar calendar = new Calendar();
         System.out.println(Time.getDate());
         summary = calendar.getTodaysScedual(Time.getDate() + "");
@@ -21,17 +21,8 @@ public class Chimes {
         for(int i = 0; i < alarmTimes.length; i++){
             alarmTimes[i] = new ArrayList<>();
         }
-
-
-        //Random Times
-        alarmTimes[4].add(32);// 0 == period 1 && 6 == period 7
-
-        while(true) {
-            Thread.sleep(100);
-            putThisInPaintLoopOnceThereIsAGUI();
-        }
     }
-    private static void putThisInPaintLoopOnceThereIsAGUI(){
+    public void putThisInPaintLoopOnceThereIsAGUI(){
         for(int i = 0; i < getAlarmTimes(getCurrentPeriod(), getCurrentBlock()).length; i++){
             System.out.println(((double)(int)((Time.getTime())*100))/100 + " : " + ((double)(int)((getAlarmTimes(getCurrentPeriod(), getCurrentBlock())[i])*100))/100);
             if(((double)(int)((Time.getTime())*100))/100 == ((double)(int)((getAlarmTimes(getCurrentPeriod(), getCurrentBlock())[i])*100))/100) {
@@ -39,7 +30,7 @@ public class Chimes {
             }
         }
     }
-    private static int getCurrentBlock(){
+    private int getCurrentBlock(){
         for(int i = 0; i < startTimes.length; i++){
             if(Time.getTime() > startTimes[i] && Time.getTime() < endTimes[i]){
                 return i;
@@ -47,7 +38,7 @@ public class Chimes {
         }
         return -1;
     }
-    private static int getCurrentPeriod(){
+    private int getCurrentPeriod(){
         for(int i = 0; i < startTimes.length; i++){
             if(Time.getTime() > startTimes[i] && Time.getTime() < endTimes[i]){
                 return days[currentDay][i];
@@ -56,7 +47,7 @@ public class Chimes {
         System.out.println("No period found");
         return -1;
     }
-    private static double[] getAlarmTimes(int period, int block){
+    private double[] getAlarmTimes(int period, int block){
         double[] alarms = new double[alarmTimes[period].size()];
         for(int i = 0; i < alarms.length; i++){
             alarms[i] = endTimes[block] - (double)alarmTimes[period].get(i)/100;
